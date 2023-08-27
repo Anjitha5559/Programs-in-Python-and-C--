4dfaae3c197a8baeda3d197eb7b917f027e768f9
Anjitha5559 <84130915+Anjitha5559@users.noreply.github.com>
2023-08-27 22:26:59 +0530

    27/08

--- repeatingelement.py
+++ repeatingelement.py
@@ -37,5 +37,19 @@ def majorityElement(array,array_length):
         return -1
 candidate = majorityElement(array,array_length)
 print(f"The majority Element is {candidate}")
-        
+#This Algorithm is called Moore's Voting algorithm
+#Eventhough it can be done through brite force, this is the most efficient method
+# Steps to implement the algorithm :
+# Step 1 – Find a candidate with the majority –
+
+# Initialize a variable say i ,votes = 0, candidate =-1 
+# Traverse through the array using for loop
+# If votes = 0, choose the candidate = arr[i] , make votes=1.
+# else if the current element is the same as the candidate increment votes
+# else decrement votes.
+# Step 2 – Check if the candidate has more than N/2 votes –
+
+# Initialize a variable count =0 and increment count if it is the same as the candidate.
+# If the count is >N/2, return the candidate.
+# else return -1.      
     
