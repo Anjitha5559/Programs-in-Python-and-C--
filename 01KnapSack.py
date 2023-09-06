820cc3693401097862234c7d1242175b377ec3e6
Anjitha5559 <84130915+Anjitha5559@users.noreply.github.com>
2023-09-06 07:54:02 +0530

    06/09

--- 01KnapSack.py
+++ 01KnapSack.py
@@ -1,19 +1,55 @@
-n = int(input("Plz Enter the Number of Objects :"))
-W = int(input("Plz Enter the weight of the sack"))
-arr = []
-for i in range(n):
-    elements = int(input("Plz Enter the elements"))
-    arr.append(elements)
-def knapSack(W,wt,n,val):
-    dp = [[0]*(W+1) for _ in range(n+1)]
-    for i in range(1,n):
-        for w in range(1,w):
-            if wt[i-1]<=W:
-                dp[n][w] = max(val(i-1)+ dp[i-1][w-wt[i]],dp[i-1][w])
-            else:
-                dp[n][w]  =dp[i-1][w]
-    return dp[n][w]
-dp = knapSack(W,wt,n,val)
-
-        
+class Solution:
     
+    #Function to return max value that can be put in knapsack of capacity W.
+    def knapSack(self,W, wt, val, n):
+        a = [[0]* (W+1) for _ in range(n+1)]
+        for i in range(1,n+1):
+            for j in range(1,W+1):
+                if i==0 or j == 0:
+                    a[i][j] = 0
+                elif wt[i-1] <= j:
+                    a[i][j] = max(a[i-1][j],a[i-1][j-wt[i-1]]+val[i-1])
+                else:
+                    a[i][j] = a[i-1][j]
+        return a[i][j]
+        
+    # def knapSack(self, W, wt, val, n):
+    #     a = [[0] * (W + 1) for _ in range(n + 1)]
+    #     for i in range(1, n + 1):
+    #         for j in range(1, W + 1):
+    #             if i == 0 or j == 0:
+    #                 a[i][j] = 0
+    #             elif wt[i - 1] <= j:
+    #                 a[i][j] = max(a[i - 1][j], val[i - 1] + a[i - 1][j - wt[i - 1]])
+    #             else:
+    #                 a[i][j] = a[i - 1][j]
+    #     return a[n][W]
+
+
+            
+            
+        
+       
+        # code here
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+import atexit
+import io
+import sys
+
+# Contributed by : Nagendra Jha
+
+if __name__ == '__main__':
+    test_cases = int(input())
+    for cases in range(test_cases):
+        n = int(input())
+        W = int(input())
+        val = list(map(int,input().strip().split()))
+        wt = list(map(int,input().strip().split()))
+        ob=Solution()
+        print(ob.knapSack(W,wt,val,n))
+# } Driver Code Ends
+#https://www.youtube.com/watch?v=PfkBS9qIMRE&list=PLdo5W4Nhv31aBrJE1WS4MR9LRfbmZrAQu&index=5
